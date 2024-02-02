ddfbc455dbda8fd27da884a6618835fba15d9a93
krishnabc <krishnabc134@gmail.com>
2024-02-02 17:18:33 +0530

    MODELS making of account app

--- webquizzy/account/models.py
+++ webquizzy/account/models.py
@@ -1,7 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
-    username = models.CharField(max_length=255)
+    user = models.OneToOneField(User, on_delete=models.CASCADE , null=True)#once the user is deleted it will delete it whole profile
+    email_add = models.CharField(max_length=100, unique=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    profile_img = models.ImageField(upload_to='profile_images', default='user.png', blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+    GENDER = (
+        ('Male','Male'),
+        ('Female','Female'),
+        ('Other','Other'),
+    )
+    gender = models.CharField(max_length=6, choices=GENDER, blank=True, null=True)
+# after creating models make sure to register in admin.py in same app file
+
 # these are the schema declaration credientials which we have to specify in model to use in database
 
--- webquizzy/webquizzy/urls.py
+++ webquizzy/webquizzy/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include # must to include urls from other app files
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')), # use to tell main django file that their is a app file in  which some urls are present
 ]
+
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
